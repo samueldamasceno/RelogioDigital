b0c2534c4a5ae1dadcc1b564624a23aefac7a78f
s-damasceno <samueldamasceno.vip@gmail.com>
2024-06-09 21:40:17 -0300

    opção de mudar cor do fundo adicionada

--- relógio digital/script.js
+++ relógio digital/script.js
@@ -24,3 +24,40 @@ const relogio = setInterval(function time() {
     minutos.textContent = min;
     segundos.textContent = s;
 })
+
+const botaoMudarCor = document.getElementById('mudar-tema-btn');
+
+const cores = [
+    'linear-gradient(120deg, #fafa6e, #63c887)',
+    'linear-gradient(120deg, #ff9a9e, #fad0c4)',
+    'linear-gradient(120deg, #a18cd1, #fbc2eb)',
+    'linear-gradient(120deg, #ffecd2, #fcb69f)',
+    'linear-gradient(120deg, #ff9a9e, #fecfef)',
+    'linear-gradient(120deg, #ff9a9e, #fad0c4)',
+    'linear-gradient(120deg, #a18cd1, #fbc2eb)',
+    'linear-gradient(120deg, #ffecd2, #fcb69f)',
+    'linear-gradient(120deg, #fdfbfb, #ebedee)',
+    'linear-gradient(120deg, #cfd9df, #e2ebf0)',
+    'linear-gradient(120deg, #ff9a9e, #fecfef)',
+    'linear-gradient(120deg, #f6d365, #fda085)',
+    'linear-gradient(120deg, #fbc2eb, #a6c1ee)',
+    'linear-gradient(120deg, #fdcbf1, #e6dee9)',
+    'linear-gradient(120deg, #a1c4fd, #c2e9fb)',
+    'linear-gradient(120deg, #232526, #414345)',
+    'linear-gradient(120deg, #0f2027, #203a43, #2c5364)',
+    'linear-gradient(120deg, #2b5876, #4e4376)',
+    'linear-gradient(120deg, #141e30, #243b55)',
+    'linear-gradient(120deg, #000000, #434343)',
+    'linear-gradient(120deg, #ff6e7f, #bfe9ff)',
+    'linear-gradient(120deg, #56ab2f, #a8e063)',
+    'linear-gradient(120deg, #ff758c, #ff7eb3)',
+    'linear-gradient(120deg, #cc2b5e, #753a88)',
+    'linear-gradient(120deg, #fc466b, #3f5efb)',
+];
+
+let corAtual = 0;
+
+botaoMudarCor.addEventListener('click', function() {
+    corAtual = (corAtual + 1) % cores.length;
+    document.body.style.background = cores[corAtual];
+});
